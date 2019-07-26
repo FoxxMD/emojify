51c3e27676e636aa09140f5ce8c4c62626bf3940
FoxxMD <FoxxMD@users.noreply.github.com>
2019-07-26 13:22:09 -0400

    Updates

---
+++ precache-manifest.809e6f89b6f689d3314b74dd0a925547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2b70d956aa7ed9976abd013ca8886f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37fd771b05ecaf1aec1e",
+    "url": "/static/css/main.6b140e79.chunk.css"
+  },
+  {
+    "revision": "c27e74bba0d0e89ffb05",
+    "url": "/static/js/2.97faa2e5.chunk.js"
+  },
+  {
+    "revision": "37fd771b05ecaf1aec1e",
+    "url": "/static/js/main.1dca238a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
